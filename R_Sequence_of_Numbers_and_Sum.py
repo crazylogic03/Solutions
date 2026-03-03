6c141d7fc23311dd6ddae552d76c824f53892f01
crazylogic03 <saithrishul2006@gmail.com>
2026-03-03 16:24:49 +0530

    Sequence

---
+++ R_Sequence_of_Numbers_and_Sum.py
@@ -0,0 +1,12 @@
+while True:
+    N, M = map(int, input().split())
+    if N <= 0 or M <= 0:
+        break
+    start = min(N, M)
+    end = max(N, M)
+    total = 0
+    numbers = []
+    for i in range(start, end + 1):
+        numbers.append(str(i))
+        total += i
+    print(" ".join(numbers) + f" sum ={total}")
