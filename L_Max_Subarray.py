0a7a549e097aa0567088c0a3914b741ff130355e
crazylogic03 <saithrishul2006@gmail.com>
2026-03-12 21:35:33 +0530

    Max Subarray

---
+++ L_Max_Subarray.py
@@ -0,0 +1,11 @@
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    arr = list(map(int, input().split()))
+    res = []
+    for i in range(n):
+        current_max = arr[i]
+        for j in range(i, n):
+            current_max = max(current_max, arr[j])
+            res.append(current_max)
+    print(*res)
