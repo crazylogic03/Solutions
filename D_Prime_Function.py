6d6ebce71e73354676aad6cbb08a920f11b2ab8c
crazylogic03 <saithrishul2006@gmail.com>
2026-04-17 23:42:32 +0530

    Prime Function

---
+++ D_Prime_Function.py
@@ -0,0 +1,16 @@
+import math
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            return False
+    return True
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    if is_prime(n):
+        print("YES")
+    else:
+        print("NO")
